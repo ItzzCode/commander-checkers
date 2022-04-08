4f9294c117929332e531e2a4483073d63c4c50c5
ItzzCode <51547168+ItzzCode@users.noreply.github.com>
2022-04-08 00:20:55 -0400

    bup

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@
 */
 
 #include <graphx.h>
+#include <tice.h>
 #include <debug.h>
 
 bool partial_redraw = false;
@@ -31,14 +32,32 @@ void end(){
     //TODO
 }
 
+//handle idk yet
 bool step(){
-    return true;
+    if ( os_GetKey() == 45 ){
+        return false;
+    } else {
+        return true;
+    }
 }
 
 //handle graphics
 void draw(){
+    //draw the board
     gfx_FillScreen(255);
-    dbg_sprintf(dbgout,"in draw");
+    for( int i = 0; i < 8; i++ ){
+        for( int e = 0; e < 8; e++ ){
+            if ( (i+e)%2 == 0){
+                gfx_SetColor(128);
+            } else {
+                gfx_SetColor(127);
+            }
+            gfx_FillRectangle(xOffset+i*16, yOffset+e*16, 18, 18);
+        }
+    }
+
+    //draw the pieces
+    dbg_sprintf(dbgout,"in draw\n");
     //go through every piece in board
     for( int i=0; i < sizeof(board) / 5; i++ ){
         //determine what color to use for a piece
@@ -48,26 +67,26 @@ void draw(){
             gfx_SetColor(0);
         }
         //piece
-        gfx_FillCircle(board[i][0]*16+xOffset,board[i][1]*16+yOffset,8);
+        gfx_FillCircle(board[i][0]*16+xOffset+8,board[i][1]*16+yOffset+8,8);
     }
 }
 
 
 int main( void ) {
-    dbg_sprintf(dbgout,"began main");
+    dbg_sprintf(dbgout,"began main\n");
     gfx_SetColor(0);
     gfx_Begin();
     gfx_SetDrawBuffer();
     gfx_FillScreen(255);
 
     while (step()) {
-        dbg_sprintf(dbgout,"in loop");
+        dbg_sprintf(dbgout,"in loop\n");
         if (partial_redraw){
             gfx_BlitScreen();
         }
         draw(); 
         
-        dbg_sprintf(dbgout,"swap draw");
+        dbg_sprintf(dbgout,"swap draw\n");
         gfx_SwapDraw(); 
     }
     return 0;
