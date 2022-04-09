a6b7a5a3896369e7622a1abb61b582cd3ac6d13f
ItzzCode <51547168+ItzzCode@users.noreply.github.com>
2022-04-08 20:31:18 -0400

    this is good! moving checkers now works.

--- src/main.cpp
+++ src/main.cpp
@@ -24,17 +24,33 @@ uint8_t board[24][5] = {
 };
 int xOffset = 160-64;
 int yOffset = 120-64;
-uint8_t select[2] = {0,0}; //x,y
+uint8_t select;
+uint8_t selectedChecker;
+uint8_t moveSelect[2];
+bool hasSelected = false;
 
+/**
+ * @brief TODO
+ * 
+ */
 void begin(){
     //TODO
 }
 
+/**
+ * @brief TODO
+ * 
+ */
 void end(){
     //TODO
 }
 
-//handle logic
+/**
+ * @brief handle logic
+ * 
+ * @return true 
+ * @return false 
+ */
 bool step(){
     dbg_sprintf(dbgout, "inside of step()\n");
 
@@ -43,26 +59,44 @@ bool step(){
     if( !kb_IsDown(kb_KeyClear) ){
         dbg_sprintf(dbgout, "user did not exit\n");
 
-        //input handling :>
+        //input handling (and bracket hell) :>
         if( kb_IsDown(kb_Key2nd) ){
-
+            if( !hasSelected ){
+                selectedChecker = select;
+                hasSelected = true;
+            } else {
+                board[select][0] = moveSelect[0];
+                board[select][1] = moveSelect[1];
+                hasSelected = false;
+            }
+        } else if( kb_IsDown(kb_KeyAlpha) ){
+            hasSelected = false;
+        }else if( hasSelected ){
+            if( kb_IsDown(kb_KeyLeft) ){
+                moveSelect[0]--;
+            } else if( kb_IsDown(kb_KeyRight) ){
+                moveSelect[0]++;
+            } else if( kb_IsDown(kb_KeyUp) ){
+                moveSelect[1]--;
+            } else if( kb_IsDown(kb_KeyDown) ){
+                moveSelect[1]++;
+            }
         } else if( kb_IsDown(kb_KeyLeft) ){
-            select[0]--;
+            select--;
         } else if( kb_IsDown(kb_KeyRight) ){
-            select[0]++;
-        } else if( kb_IsDown(kb_KeyUp) ){
-            select[1]--;
-        } else if( kb_IsDown(kb_KeyDown) ){
-            select[1]++;
+            select++;
         }
-    
+        delay(60);
         return true;
     } else {
         return false;
     }
 }
 
-//handle graphics
+/**
+ * @brief handle graphics
+ * 
+ */
 void draw(){
     //draw the board
     gfx_FillScreen(255);
@@ -73,15 +107,15 @@ void draw(){
             } else {
                 gfx_SetColor(128);
             }
-            gfx_FillRectangle(xOffset+i*16, yOffset+e*16, 18, 18);
+            gfx_FillRectangle(xOffset+i*16, yOffset+e*16, 16, 16);
         }
     }
 
     //draw the pieces
     dbg_sprintf(dbgout,"in draw\n");
     //go through every piece in board
-    for( int i=0; i < sizeof(board) / 5; i++ ){
-        //determine what color to use for a piece
+    for( int i=0; i < (int)sizeof(board) / 5; i++ ){
+        //determine what color to use for a piece 
         if ( board[i][3] ) {
             gfx_SetColor(224);
         } else {
@@ -93,18 +127,32 @@ void draw(){
 
     //just say what select is
     gfx_PrintStringXY("Selected:",0,0);
-    gfx_PrintUInt(select[0], 1);
-    gfx_PrintString(" ");
-    gfx_PrintUInt(select[1], 1);
+    gfx_PrintUInt(select, 1);
 
-    //show selected checker
-    gfx_SetColor(196);
-    gfx_Rectangle(xOffset+select[0]*16, yOffset+select[1]*16, 16, 16);
-    //this seems quite odd and i think there's a better way to do this
-    gfx_Rectangle(xOffset+select[0]*16-1, yOffset+select[1]*16-1, 18, 18); 
+    //show select box
+    if ( hasSelected ){
+        gfx_SetColor(6);
+        gfx_Rectangle(xOffset+moveSelect[0]*16, yOffset+moveSelect[1]*16, 16, 16);
+        //this seems quite odd and i think there's a better way to do this
+        gfx_Rectangle(xOffset+moveSelect[0]*16-1, yOffset+moveSelect[1]*16-1, 18, 18); 
+    } else {
+        gfx_SetColor(196);
+        gfx_Rectangle(board[select][0]*16+xOffset,board[select][1]*16+yOffset,16,16);
+        //this also seems quite odd and i think there's a better way to do this
+        gfx_Rectangle(board[select][0]*16-1+xOffset,board[select][1]*16-1+yOffset,18,18);
+    }
+
+    //show selected checker (copy paste of previous code, only with selectedChecker as the coords)
+    gfx_SetColor(168);
+    gfx_Rectangle(board[selectedChecker][0]*16+xOffset,board[selectedChecker][1]*16+yOffset,16,16);
+    gfx_Rectangle(board[selectedChecker][0]*16-1+xOffset,board[selectedChecker][1]*16-1+yOffset,18,18);
 }
 
-
+/**
+ * @brief main
+ * 
+ * @return int 
+ */
 int main( void ) {
     dbg_sprintf(dbgout,"began main\n");
     gfx_SetColor(0);
